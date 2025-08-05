a12cb77c4dd3733ee322afd93f7250a58917e8ad
mannptl215 <mannptl2152work@gmail.com>
2025-08-05 20:36:06 +0530

    Update Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -172,7 +172,7 @@ const Signup = () => {
       <div className="w-full max-w-md">
         <div className="text-center mb-8">
           <img 
-            src="/lovable-uploads/84003fdb-4481-45db-89c7-ef29b876e712.png" 
+            src="/lovable-uploads/84003fdb-4481-45db-89c7-ef29b876e71200.png" 
             alt="VIZMORA" 
             className="h-10 w-auto mx-auto mb-4"
           />
@@ -399,3 +399,4 @@ const Signup = () => {
 };
 
 export default Signup;
+
